1b1e964c09713fc01d9ee0f06925e01a24e42eab
baoduongthien <baoduongthien.it@gmail.com>
2022-10-23 09:35:49 +0700

    mobile menu responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,10 @@ const modalElement = document.querySelector('.js-modal');
 const closeModal = document.querySelector('.js-modal-close');
 const modalContainerElement = document.querySelector('.js-modal-container');
 
+const headerElement = document.querySelector('#header');
+const menuIconElement = document.querySelector('#mobile-menu');
+const menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+
 for (const buyBtn of buyBtns) {
     buyBtn.onclick = function() {
         modalElement.classList.add('open');
@@ -19,4 +23,29 @@ modalElement.onclick = function() {
 
 modalContainerElement.onclick = function(event) {
     event.stopPropagation();
+}
+
+const headerHeight = headerElement.clientHeight;
+
+menuIconElement.onclick = function() {
+    const isClosed = headerHeight === headerElement.clientHeight;
+
+    if (isClosed) {
+        headerElement.style.height = 'auto';
+    } else {
+        headerElement.style.height = null;
+    }
+}
+
+for (const menuItem of menuItems) {
+    const isParentMenu = menuItem.nextElementSibling && menuItem.nextElementSibling.classList.contains('subnav');
+    
+    menuItem.onclick = function(event) {
+        if (!isParentMenu) {
+            headerElement.style.height = null;
+        } else {
+            event.preventDefault();
+        }
+
+    }
 }
